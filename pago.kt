e970a271d5ede2dd06bf7a3eefc3017393c87f28
DANNY CEDEÑO <dannycedeno823@gmail.com>
2026-08-09 22:28:16 -0500

    Create pago.kt

---
+++ pago.kt
@@ -0,0 +1,17 @@
+package ec.edu.istg.gestioneventos.model
+
+data class Pago(
+
+    var idPago: Int = 0,
+
+    var idCliente: Int,
+
+    var monto: Double,
+
+    var fechaPago: String,
+
+    var metodoPago: String,
+
+    var estadoPago: String
+
+)
